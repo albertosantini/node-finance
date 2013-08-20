cf4d4fe38bbc44787c474b7bc2951bbbd160c709
icebox <albertosantini@gmail.com>
2013-08-20 21:37:28 +0200

    Add option chain test.

---
+++ test/option-chain-test.js
@@ -0,0 +1,30 @@
+"use strict";
+
+var vows = require("vows"),
+    assert = require("assert"),
+    optionChain = require("../lib/option-chain");
+
+vows.describe("Option Chain tests").addBatch({
+    "get option chain from YAHOO": {
+        topic: function () {
+            optionChain.getOptionChainFromYahoo("IBM", this.callback);
+        },
+
+        "strike is a number": function (topic) {
+            assert.ok(isFinite(topic.strike));
+        },
+
+        // "expire date": function (topic) {
+        //     assert.ok(isFinite(topic.calls[0]));
+        // },
+
+        "first call strike is a number": function (topic) {
+            assert.ok(isFinite(topic.calls[0].strike));
+        },
+
+        "first put strike is a number": function (topic) {
+            assert.ok(isFinite(topic.puts[0].strike));
+        }
+    }
+
+}).export(module);
