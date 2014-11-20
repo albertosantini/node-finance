eabe12e9e73e319584a574a650c9acc6ba78821f
icebox <albertosantini@gmail.com>
2014-11-20 18:32:18 +0100

    Add Gruntfile.js.

---
+++ Gruntfile.js
@@ -0,0 +1,35 @@
+module.exports = function (grunt) {
+    "use strict";
+
+    grunt.initConfig({
+        eslint: {
+            options: {
+                config: ".eslintrc"
+            },
+            src: [
+                "Gruntfile.js",
+                "index.js",
+                "lib/**/*.js",
+                "test/**/*-test.js"
+            ]
+        },
+
+        vows: {
+            all: {
+                src: "test/*-test.js",
+                options: {
+                    reporter: "spec",
+                    error: false
+                }
+            }
+        }
+    });
+
+    grunt.loadNpmTasks("grunt-eslint");
+    grunt.loadNpmTasks("grunt-vows-runner");
+
+    grunt.registerTask("default", [
+        "eslint",
+        "vows"
+    ]);
+};
