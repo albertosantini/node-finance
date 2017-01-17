f21f9a1049663b0e9914216e9970a6115ad47bb2
Alberto Santini <albertosantini@gmail.com>
2017-01-17 10:18:42 +0100

    Fix typo in key-statistics example.

--- examples/key-statistic.js
+++ examples/key-statistic.js
@@ -5,5 +5,5 @@ var keyStats = require("../lib/key-statistics");
 keyStats.getKeyStatistics({ symbol: "IBM" }, function (err, stats) {
     if (!err) {
         console.log(stats);
-    }    
-);
+    }
+});
