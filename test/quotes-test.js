e6c7e80b4b3c5b660beb615b2de1d602c145d320
icebox <albertosantini@gmail.com>
2018-06-19 17:30:37 +0200

    Improve error handling in quotes.

--- test/quotes-test.js
+++ test/quotes-test.js
@@ -5,20 +5,22 @@ const test = require("tape");
 const quotes = require("../lib/quotes");
 
 test("Quotes tests", t => {
-    t.plan(4);
+    t.plan(8);
 
-    quotes.getQuotes(["YHOO"], new Date("Sat Aug 06 2011 12:00:00"),
-        (err, res) => {
-            t.ok(!err && res[0].beforeRefDate.length > 0,
-                "get quotes before ref date in the past");
-            t.ok(!err && res[0].afterRefDate.length > 0,
-                "get quotes after ref date in the past");
-        });
+    quotes.getQuotes(["YHOO"], new Date("Sat Aug 06 2011 12:00:00"), (err, res) => {
+        t.equal(err, "Not Found", "not found for unknown symbol");
+        t.notOk(res[0], "no data for unknown symbol");
+    });
 
-    quotes.getQuotes(["YHOO"], new Date(), (err, res) => {
-        t.ok(!err && res[0].beforeRefDate.length > 0,
-            "get quotes before today");
-        t.ok(!err && res[0].afterRefDate.length === 0,
-            "get quotes after today");
+    quotes.getQuotes(["IBM"], new Date("Sat Aug 06 2011 12:00:00"), (err, res) => {
+        t.notOk(err, "check error for getting quotes before ref date in the past");
+        t.ok(res[0].beforeRefDate.length > 0, "get quotes before ref date in the past");
+        t.ok(res[0].afterRefDate.length > 0, "get quotes after ref date in the past");
+    });
+
+    quotes.getQuotes(["IBM"], new Date(), (err, res) => {
+        t.notOk(err, "check error for getting quotes before today");
+        t.ok(res[0].beforeRefDate.length > 0, "get quotes before today");
+        t.ok(res[0].afterRefDate.length === 0, "get quotes after today");
     });
 });
