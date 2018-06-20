f4798d67e16ab24f64c947ca2e6eff96ab92bc06
icebox <albertosantini@gmail.com>
2018-06-20 07:41:13 +0200

    Improve error handling in returns.

--- test/returns-test.js
+++ test/returns-test.js
@@ -5,32 +5,33 @@ const test = require("tape");
 const returns = require("../lib/returns");
 
 test("Returns tests", t => {
-    t.plan(9);
+    t.plan(15);
 
-    returns.getReturns(["GOOGL", "IBM"], new Date("Sat Aug 06 2011 12:00:00"),
-        (err, res) => {
+    returns.getReturns(["YHOO"], new Date("Sat Aug 06 2011 12:00:00"), (err, res) => {
+        t.equal(err, "Not Found", "not found for unknown symbol");
+        t.notOk(res, "no data for unknown symbol");
+    });
 
-            t.ok(!err && res.beforeRefDate.length === 2,
-                "get returns for two assets before ref date in the past");
-            t.ok(!err && res.beforeRefDate[0].length > 0,
-                "get returns for first assets before ref date in the past");
-            t.ok(!err && res.beforeRefDate[1].length > 0,
-                "get returns for second assets before ref date in the past");
-            t.ok(!err && res.afterRefDate[0].length > 0,
-                "get returns for first assets after ref date in the past");
-            t.ok(!err && res.afterRefDate[1].length > 0,
-                "get returns for second assets after ref date in the past");
-        });
+    returns.getReturns(["YHOO", "IBM"], new Date("Sat Aug 06 2011 12:00:00"), (err, res) => {
+        t.equal(err, "Not Found", "not found for mixed unknown symbol");
+        t.notOk(res, "no data for mixed unknown symbol");
+    });
+
+    returns.getReturns(["GOOGL", "IBM"], new Date("Sat Aug 06 2011 12:00:00"), (err, res) => {
+        t.notOk(err, "check error for getting returns before ref date in the past");
+        t.ok(res.beforeRefDate.length === 2, "get returns for two assets before ref date in the past");
+        t.ok(res.beforeRefDate[0].length > 0, "get returns for first assets before ref date in the past");
+        t.ok(res.beforeRefDate[1].length > 0, "get returns for second assets before ref date in the past");
+        t.ok(res.afterRefDate[0].length > 0, "get returns for first assets after ref date in the past");
+        t.ok(res.afterRefDate[1].length > 0, "get returns for second assets after ref date in the past");
+    });
 
     returns.getReturns(["GOOGL", "IBM"], new Date(), (err, res) => {
-        t.ok(!err && res.beforeRefDate.length === 2,
-            "get returns for two assets before today");
-        t.ok(!err && res.beforeRefDate[0].length > 0,
-            "get returns for first assets before today");
-        t.ok(!err && res.beforeRefDate[1].length > 0,
-            "get returns for second assets before today");
-        t.ok(!err && res.afterRefDate.length === 0,
-            "get returns after today");
+        t.notOk(err, "check error for getting returns before today");
+        t.ok(res.beforeRefDate.length === 2, "get returns for two assets before today");
+        t.ok(res.beforeRefDate[0].length > 0, "get returns for first assets before today");
+        t.ok(res.beforeRefDate[1].length > 0, "get returns for second assets before today");
+        t.ok(res.afterRefDate.length === 0, "get returns after today");
     });
 
 });
