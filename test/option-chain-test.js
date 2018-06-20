fc9f605163cd49c8c54c2046ab2774a1af6c0f73
icebox <albertosantini@gmail.com>
2018-06-20 07:39:59 +0200

    Improve error handling in option chain.

--- test/option-chain-test.js
+++ test/option-chain-test.js
@@ -5,36 +5,32 @@ const test = require("tape");
 const optionChain = require("../lib/option-chain");
 
 test("Option Chain tests", t => {
-    t.plan(8);
+    t.plan(10);
 
     optionChain.getOptionChainFromYahoo({
         symbol: "IBM"
     }, (err, res) => {
-        const expiration = !err && res.expDate.toString(),
-            date = new Date(expiration),
-            dateStr = `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`;
+        t.notOk(err, "check error for getting option chain");
 
-        t.ok(!err && isFinite(res.strike),
-            "strike is a number");
-        t.ok(!err && expiration !== "Invalid Date",
-            "expire date");
-        t.ok(!err && isFinite(res.calls[0].strike),
-            "first call strike is a number");
-        t.ok(!err && isFinite(res.puts[0].strike),
-            "first put strike is a number");
+        const expiration = res.expDate.toString();
+        const date = new Date(expiration);
+        const dateStr = `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`;
+
+        t.ok(isFinite(res.strike), "strike is a number");
+        t.ok(expiration !== "Invalid Date", "expire date");
+        t.ok(isFinite(res.calls.length && res.calls[0].strike), "first call strike is a number");
+        t.ok(isFinite(res.puts.length && res.puts[0].strike), "first put strike is a number");
 
         optionChain.getOptionChainFromYahoo({
             symbol: "IBM",
             expiration: dateStr
         }, (err2, res2) => {
-            t.ok(!err2 && isFinite(res2.strike),
-                "strike is a number with expiration date");
-            t.ok(!err2 && res2.expDate.toString() !== "Invalid Date",
-                "expire date with expiration date");
-            t.ok(!err2 && isFinite(res2.calls[0].strike),
-                "first call strike is a number with expiration date");
-            t.ok(!err2 && isFinite(res2.puts[0].strike),
-                "first put strike is a number with expiration date");
+            t.notOk(err2, "check error for getting option chain with expiration date");
+
+            t.ok(isFinite(res2.strike), "strike is a number with expiration date");
+            t.ok(res2.expDate.toString() !== "Invalid Date", "expire date with expiration date");
+            t.ok(isFinite(res2.calls.length && res2.calls[0].strike), "first call strike is a number with expiration date");
+            t.ok(isFinite(res2.puts.length && res2.puts[0].strike), "first put strike is a number with expiration date");
         });
     });
 });
